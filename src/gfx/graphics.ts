f9a1f339b848f14007dec33c5033e163f9e66437
Steven Cohen <peragwin@gmail.com>
2020-06-25 08:16:02 -0700

    more wip on compute shader

--- src/gfx/graphics.ts
+++ src/gfx/graphics.ts
@@ -131,7 +131,7 @@ export class VertexArrayObject {
   }
 }
 
-abstract class RenderTarget {
+export abstract class RenderTarget {
   abstract use(): void;
 }
 
--- src/gfx/pps/countingshader.ts
+++ src/gfx/pps/countingshader.ts
@@ -1,4 +1,9 @@
-import { ShaderConfig } from "../graphics";
+import {
+  ShaderConfig,
+  Graphics,
+  RenderTarget,
+  TextureObject,
+} from "../graphics";
 
 const countingShader = (
   gl: WebGL2ComputeRenderingContext,
@@ -24,16 +29,29 @@ uniform ivec2 uStateSize;
 //   int y;
 // };
 
-layout (std140, binding = 0) buffer ivec2 bufPositions[];
-layout (std140, binding = 1) buffer ivec2 bufSortedPositions[];
-layout (std140, binding = 2) buffer ivec2 bufPositionCounts[];
+// layout (std140, binding = 0) buffer PositionBuffer {
+//   ivec2 data[];
+// } ssboPosition;
+
+// layout (std140, binding = 1) buffer SortedPositions {
+//   ivec2 data[];
+// } ssboSortedPosition;
+
+// layout (std140, binding = 2) buffer PositionCounts {
+//   ivec2 data[];
+// } ssboPositionCount;
+
+layout (rg32i, binding = 0) uniform coherent readonly restrict image2D imgPosition;
+layout (rg32i, binding = 1) uniform coherent writeonly restrict image2D imgSortedPosition;
+layout (rg32i, binding = 2) uniform coherent writeonly restrict image2D imgPositionCount;
 
 shared int counts[${gridSize * gridSize}];
 shared int totals[${gridSize * gridSize}];
 shared int subTotals[GROUP_SIZE];
 
 int positionToIndex(in int index) {
-  ivec2 pi = bufPositions[index];
+  // ivec2 pi = ssboPosition.data[index];
+  ivec2 pi = imageLoad()
   vec2 p = vec2(intBitsToFloat(p.x), intBitsToFloat(p.y));
   p = (p + 1.) / 2.;
   ivec2 gi = ivec2(floor(p * float(GRID_SIZE)));
@@ -74,13 +92,13 @@ void sortPositions(in ivec2 seg) {
   for (int i = seg.s; i < seg.t; i++) {
     const idx = positionToIndex(i);
     const total = atomicAdd(totals[idx], 1);
-    bufSortedPositions[total] = bufPositions[i];
+    ssboSortedPosition.data[total] = ssboPosition.data[i];
   }
 }
 
 void writeCounts(in ivec2 seg, in int threadIndex) {
   for (int i = seg.s; i < seg.t; i++) {
-    bufPositionCounts[i] = ivec2(counts[i], totals[i]);
+    ssboPositionCount.data[i] = ivec2(counts[i], totals[i]);
   }
 }
 
@@ -131,14 +149,96 @@ void main () {
   return new ShaderConfig(countingShaderSrc, gl.COMPUTE_SHADER, [], []);
 };
 
+class ComputeTarget extends RenderTarget {
+  constructor() {
+    super();
+  }
+
+  public use() {}
+}
+
+class ShaderBufferObject {
+  private buffer: WebGLBuffer;
+  constructor(private gl: WebGL2ComputeRenderingContext) {
+    const buffer = gl.createBuffer();
+    if (!buffer) {
+      throw new Error("failed to create buffer");
+    }
+    this.buffer = buffer;
+  }
+
+  public bind(id: number) {
+    this.gl.bindBufferBase(this.gl.SHADER_STORAGE_BUFFER, id, this.buffer);
+  }
+}
+
 interface StateSize {
   width: number;
   height: number;
 }
 
+interface Textures {
+  position: TextureObject;
+  sortedPosition: TextureObject;
+  positionCount: TextureObject;
+}
+
 export class CountingSortComputer {
+  private gfx: Graphics;
+  private ssboPosition: ShaderBufferObject;
+  private ssboSortedPosition: ShaderBufferObject;
+  private ssboPositionCount: ShaderBufferObject;
+
+  private readonly workGroupSize = 1024;
+
   constructor(
     private gl: WebGL2ComputeRenderingContext,
-    private stateSize: StateSize
-  ) {}
+    private stateSize: StateSize,
+    private gridSize: number,
+    private textures: Textures
+  ) {
+    const workGroupSize = this.workGroupSize;
+    const ss = stateSize.width * stateSize.height;
+    if (ss % workGroupSize !== 0) {
+      throw new Error(
+        `stateSize x*y ${ss} must be divisible by ${workGroupSize}`
+      );
+    }
+    const gg = gridSize * gridSize;
+    if (gg % workGroupSize !== 0) {
+      throw new Error(
+        `gridSize**2 ${gg} must be divisible by ${workGroupSize}`
+      );
+    }
+
+    const tgt = new ComputeTarget();
+    const shaders = [countingShader(gl, gridSize, workGroupSize)];
+    const gfx = new Graphics(gl, tgt, shaders, this.onCompute.bind(this));
+    this.gfx = gfx;
+
+    gfx.attachUniform("uStateSize", (l, v: StateSize) =>
+      gl.uniform2f(l, v.width, v.height)
+    );
+
+    this.ssboPosition = new ShaderBufferObject(gl);
+    this.ssboSortedPosition = new ShaderBufferObject(gl);
+    this.ssboPositionCount = new ShaderBufferObject(gl);
+  }
+
+  private onCompute() {
+    // 1. Copy position texture to ssboPosition
+    this.gl.copyBufferSubData;
+
+    // 2. Bind buffers and uniform
+    this.ssboPosition.bind(0);
+    this.ssboSortedPosition.bind(1);
+    this.ssboPositionCount.bind(2);
+    this.gfx.bindUniform("uStateSize", this.stateSize);
+
+    // 3. Execute compute shader
+    this.gl.dispatchCompute(1, 1, 1);
+    this.gl.memoryBarrier(this.gl.SHADER_STORAGE_BARRIER_BIT);
+
+    // 4. Copy output buffers to textures
+  }
 }
