8cb4cdd7b10c50de9ff8a807a96e54a9623760a4
Steven Cohen <peragwin@gmail.com>
2020-07-12 19:02:59 -0700

    improve main splash page

--- src/components/EntryPoint.tsx
+++ src/components/EntryPoint.tsx
@@ -3,16 +3,21 @@ import { useHistory } from "react-router-dom";
 
 import { makeStyles } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import IconButton from "@material-ui/core/IconButton";
 
-export const useStyles = makeStyles({
-  buttonContainer: {
-    width: "100vw",
+export const useStyles = makeStyles((theme) => ({
+  title: {
+    margin: "auto",
     textAlign: "center",
+    fontFamily: "'Iceland', cursive",
   },
-  buttonsJustified: {
-    display: "inline-grid",
-    width: "100vw",
-    maxWidth: "400px",
+  footer: {
+    textAlign: "center",
+    marginTop: theme.spacing(8),
   },
   button: {
     background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
@@ -24,8 +29,33 @@ export const useStyles = makeStyles({
     padding: "0 30px",
     margin: "1.5em",
     maxWidth: "400px",
+    width: "100vw",
   },
-});
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    "& > *": {
+      margin: "auto",
+    },
+  },
+  paper: {
+    width: theme.spacing(64),
+    height: theme.spacing(64),
+    padding: theme.spacing(4),
+    textAlign: "center",
+    background: "linear-gradient(225deg, #ff22e48c -30%, #bf9bff94 100%)",
+    color: "#FFFFFF",
+    margin: theme.spacing(2),
+  },
+  cardHeading: {
+    fontFamily: "'Iceland', cursive",
+  },
+  cardInfo: {
+    margin: theme.spacing(2),
+    fontFamily: "'Poiret One', cursive",
+    fontSize: "1.25rem",
+  },
+}));
 
 interface Props {}
 
@@ -33,22 +63,76 @@ const EntryPoint: React.FC<Props> = () => {
   const classes = useStyles();
   const history = useHistory();
   return (
-    <div className={classes.buttonContainer}>
-      <div className={classes.buttonsJustified}>
-        <Button
-          className={classes.button}
-          size="large"
-          onClick={() => history.push("/pps")}
-        >
-          Particle System Simulator
-        </Button>
-        <Button
-          size="large"
-          className={classes.button}
-          onClick={() => history.push("/warp")}
-        >
-          Music Visualizer
-        </Button>
+    <div>
+      <Typography variant="h1" className={classes.title}>
+        Vuzic.app
+      </Typography>
+      <div className={classes.root}>
+        <div>
+          <Paper elevation={3} className={classes.paper}>
+            <Typography variant="h3" className={classes.cardHeading}>
+              Particle System Simulator
+            </Typography>
+            <Typography className={classes.cardInfo}>
+              The particle system is based on the Primordial Particle System
+              described in this{" "}
+              <a
+                style={{ color: "white" }}
+                href="https://www.nature.com/articles/srep37969"
+              >
+                paper
+              </a>
+              .
+            </Typography>
+            <Button
+              className={classes.button}
+              size="large"
+              onClick={() => history.push("/pps")}
+            >
+              2D Particle System
+            </Button>
+            <Typography className={classes.cardInfo}>
+              It's also possible to extend into three dimensions.
+            </Typography>
+            <Button
+              className={classes.button}
+              size="large"
+              onClick={() => history.push("/pps3")}
+            >
+              3D Particle System
+            </Button>
+          </Paper>
+        </div>
+        <div>
+          <Paper elevation={3} className={classes.paper}>
+            <Typography variant="h3" className={classes.cardHeading}>
+              Music Visualizer
+            </Typography>
+            <Typography className={classes.cardInfo}>
+              A music visualizer using a connected microphone as input.
+            </Typography>
+            <Button
+              size="large"
+              className={classes.button}
+              onClick={() => history.push("/warp")}
+            >
+              Music Visualizer
+            </Button>
+          </Paper>
+        </div>
+      </div>
+      {/* <div style={{ height: "auto" }}></div> */}
+      <div className={classes.footer}>
+        <IconButton>
+          <a style={{ color: "white" }} href="https://github.com/peragwin">
+            <GitHubIcon />
+          </a>
+        </IconButton>
+        <IconButton>
+          <a style={{ color: "white" }} href="https://instagram.com/peragwin">
+            <InstagramIcon />
+          </a>
+        </IconButton>
       </div>
     </div>
   );
